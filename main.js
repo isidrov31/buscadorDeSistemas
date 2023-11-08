c0507a110442d6dc26558a9b91eb7507917744ea
isidrov31 <127324143+isidrov31@users.noreply.github.com>
2023-11-08 19:40:08 -0300

    array de mensajes

--- main.js
+++ main.js
@@ -4,18 +4,28 @@ document.addEventListener("DOMContentLoaded", function () {
     const dropdownItems = document.querySelectorAll(".dropdown-item");
 
     const systemMessages = {
-        ASEGURAMIENTO: "👉 Si el incidente es masivo, genera un iTracker y avísanos por los medios que utilizamos usualmente (grupo de WhatsApp, llamada/mensaje por Teams). 👉 Si es particular, genera un iTracker y aguarda que nos comuniquemos a la brevedad. 👉 Si es fuera de horario laboral, genera un iTracker y llámanos a la guardia.",
-        FUNCIONAL: "En desarrollo",
+        ASEGURAMIENTO: [
+            "👉 Si el incidente es masivo, genera un iTracker y avísanos por los medios que utilizamos usualmente (grupo de WhatsApp, llamada/mensaje por Teams).",
+            "👉 Si es particular, genera un iTracker y aguarda que nos comuniquemos a la brevedad.",
+            "👉 Si es fuera de horario laboral, genera un iTracker y llámanos a la guardia."
+        ],
+        FUNCIONAL: [
+            "En desarrollo"
+        ],
     };
 
     searchButton.addEventListener("click", function () {
         const searchInput = document.getElementById("searchInput").value.trim();
-    
+
         if (searchInput === "") {
             messageContainer.innerHTML = "<p>Por favor, ingresa un sistema en el buscador.</p>";
         } else if (systemMessages.hasOwnProperty(searchInput)) {
-            const systemMessage = systemMessages[searchInput];
-            messageContainer.innerHTML = `<p>${systemMessage}</p>`;
+            const systemMessageArray = systemMessages[searchInput];
+            messageContainer.innerHTML = ""; // Limpiar el contenido existente
+
+            systemMessageArray.forEach((message, index) => {
+                messageContainer.innerHTML += `<div id="message_${searchInput}_${index}"><p>${message}</p></div>`;
+            });
         } else {
             messageContainer.innerHTML = "<p>No conozco esa aplicación 😵 😵 Inténtalo nuevamente desde la lista desplegable. Si no aparece, escrible a alguno de los siguientes referentes: Darío Panico, Cyntia Sadran, Cecilia Garcia, Debora Martini</p>";
         }
@@ -24,8 +34,12 @@ document.addEventListener("DOMContentLoaded", function () {
     dropdownItems.forEach(function (item) {
         item.addEventListener("click", function () {
             const selectedSystem = this.getAttribute("data-system");
-            const systemMessage = systemMessages[selectedSystem];
-            messageContainer.innerHTML = `<p>${systemMessage}</p>`;
+            const systemMessageArray = systemMessages[selectedSystem];
+            messageContainer.innerHTML = ""; // Limpiar el contenido existente
+
+            systemMessageArray.forEach((message, index) => {
+                messageContainer.innerHTML += `<div id="message_${selectedSystem}_${index}"><p>${message}</p></div>`;
+            });
         });
     });
 });
